5334f5b3473fb6924e89faba45d140a344f995ca
Uwe Schilling <uweschillingbw@gmail.com>
2014-01-30 14:31:33 +0100

    okey jetzt aber

--- test_sqlite.py
+++ test_sqlite.py
@@ -12,7 +12,7 @@
 
 # second argument is hosts file for name mapping
 
-# nice benchmark query is     halli hallo halloele
+# nice benchmark query is     No one simply code with git.... 
 '''
  SELECT nids.nid FROM samples
    INNER JOIN timestamps
