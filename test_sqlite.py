17ea1da86eb267925333846540e3dd04bdbd6399
Holger Berger <holger.berger@googlemail.com>
2014-01-30 14:16:18 +0100

    einer geht noch

--- test_sqlite.py
+++ test_sqlite.py
@@ -12,7 +12,7 @@
 
 # second argument is hosts file for name mapping
 
-# nice benchmark query is     halli hallo
+# nice benchmark query is     halli hallo halloele
 '''
  SELECT nids.nid FROM samples
    INNER JOIN timestamps
