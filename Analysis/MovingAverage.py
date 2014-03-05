89863f7ab88e05d952fc7e76688282845abc5d7d
Uwe Schilling <uweschillingbw@gmail.com>
2014-03-05 11:33:05 +0100

    Create folder analysis - clean up files

--- Analysis/MovingAverage.py
+++ Analysis/MovingAverage.py
@@ -6,13 +6,14 @@ Created on 13.02.2014
 
 This is a class for simple and eficient calculation of the
 moving average.  If you need the simple Average, set calcArray to [1, 1, 1]
-for the average over 3 values. 
+for the average over 3 values.
 
 '''
 
 import time
 import math
 import binomialCoeff
+
 
 class MovingAverage(object):
     def __init__(self, size = None, filterArry = None):
@@ -24,23 +25,23 @@ class MovingAverage(object):
                 calcArray = filterArry
                 size = len(filterArry)
         else:
-             calcArray = binomialCoeff.binArray(size)
-                
-        self.size = size+1
+            calcArray = binomialCoeff.binArray(size)
+
+        self.size = size + 1
         self.calcArray = calcArray
         self.calcSum = sum(calcArray)
-        self.valueList = [None]*self.size
-        self.sizeHalf = int(math.ceil(size/2))
+        self.valueList = [None] * self.size
+        self.sizeHalf = int(math.ceil(size / 2))
         self.pointPos = 0
         self.pointMin = 0
         self.pointMax = 0
         self.pointMid = 0
         self.average = []
-        
+
     def getAverage(self):
         self.calcLast()
         return self.average
-        
+
     def getValue(self, valueArray):
         tmp = 0
         for i in range(0, self.size):
@@ -53,37 +54,37 @@ class MovingAverage(object):
         #calculate only if more then the half array is filled
         if self.valueList[self.sizeHalf]:
             #state 1 -> only half array is filed
-            if not self.valueList[self.size-1]:
-                newValueArray = [None]*self.size
+            if not self.valueList[self.size - 1]:
+                newValueArray = [None] * self.size
                 valueSize = self.pointPos - self.pointMin
                 newElements = self.size - valueSize
-                
+
                 # fill with the last value
                 for i in range(0, newElements):
-                    newValueArray[i] = self.valueList[self.pointMin%self.size][1]
-                
+                    newValueArray[i] = self.valueList[self.pointMin % self.size][1]
+
                 # fill with the given values
                 tmp = self.pointMin
-                for i in range(newElements , self.size):
+                for i in range(newElements, self.size):
                     newValueArray[i] = self.valueList[tmp][1]
                     tmp += 1
-                
-                self.average.append( 
+
+                self.average.append(
                     (self.valueList[self.pointMid][0], self.getValue(newValueArray)))
 
                 self.pointMid = self.pointMid + 1
             #state 2 -> normal mode
             else:
                 newValueArray = []
-                for i in range(0 , self.size):
+                for i in range(0, self.size):
                     key = (self.pointMin + i) % self.size
                     newValueArray.append(self.valueList[key][1])
-                
-                self.average.append( 
-                    (self.valueList[self.pointMid%self.size][0], self.getValue(newValueArray)))
+
+                self.average.append(
+                    (self.valueList[self.pointMid % self.size][0], self.getValue(newValueArray)))
 
                 self.pointMid = self.pointMid + 1
-                self.pointMin = self.pointMin + 1 
+                self.pointMin = self.pointMin + 1
 
     def calcLast(self):
 
@@ -92,20 +93,20 @@ class MovingAverage(object):
             # from min to max
             if (self.pointMax - self.pointMin) >= self.size:
                 for i in range(self.pointMin, self.pointMax):
-                    newValueArray.append(self.valueList[i%self.size][1])
-                self.average.append( 
-                    (valueList[self.pointMid%self.size][0], self.getValue(newValueArray)))
+                    newValueArray.append(self.valueList[i % self.size][1])
+                self.average.append(
+                    (self.valueList[self.pointMid % self.size][0], self.getValue(newValueArray)))
                 self.pointMid = self.pointMid + 1
                 self.pointMin = self.pointMin + 1
             else:
                 for i in range(self.pointMin, self.pointMax):
-                    newValueArray.append(self.valueList[i%self.size][1])
-                
+                    newValueArray.append(self.valueList[i % self.size][1])
+
                 for k in range(len(newValueArray), self.size):
-                    newValueArray.append(self.valueList[self.pointMax%self.size][1])
-                
-                self.average.append( 
-                    (self.valueList[self.pointMid%self.size][0], self.getValue(newValueArray)))
+                    newValueArray.append(self.valueList[self.pointMax % self.size][1])
+
+                self.average.append(
+                    (self.valueList[self.pointMid % self.size][0], self.getValue(newValueArray)))
 
                 self.pointMid = self.pointMid + 1
                 self.pointMin = self.pointMin + 1
@@ -113,8 +114,8 @@ class MovingAverage(object):
     def addValue(self, timestamp, value):
         '''
             This methode calculates the filter with the values that go in.
-            
         '''
+
         # first Value
         if not self.valueList[0]:
             self.valueList[0] = (timestamp, value)
@@ -122,11 +123,11 @@ class MovingAverage(object):
 
         # after first Value
         else:
-            insertPoint = self.pointPos%self.size
+            insertPoint = self.pointPos % self.size
             self.valueList[insertPoint] = (timestamp, value)
             self.pointMax += 1
             self.pointPos += 1
-        
+
         self.calculate()
 
 
@@ -134,38 +135,39 @@ if __name__ == '__main__':
     time_start = time.time()
 #------------------------------------------------------------------------------
     mav = MovingAverage(4)
-    
     # [1, 4, 6, 4, 1]
-    p1 = 408*1 + 408*4 + 408*6 + 372*4 + 480*1
-    p1 = int(round(p1 /16))
+    p1 = 408 * 1 + 408 * 4 + 408 * 6 + 372 * 4 + 480 * 1
+    p1 = int(round(p1 / 16))
 
-    p2 = 408*1 + 408*4 + 372*6 + 480*4 + 444*1
-    p2 = int(round(p2 /16))
-    
-    p3 = 408*1 + 372*4 + 480*6 + 444*4 + 447*1
-    p3 = int(round(p3 /16))
-    
-    p4 = 372*1 + 480*4 + 444*6 + 447*4 + 492*1
-    p4 = int(round(p4 /16))
-    
-    p5 = 480*1 + 444*4 + 447*6 + 492*4 + 429*1
-    p5 = int(round(p5 /16))
+    p2 = 408 * 1 + 408 * 4 + 372 * 6 + 480 * 4 + 444 * 1
+    p2 = int(round(p2 / 16))
 
-    p6 = 444*1 + 447*4 + 492*6 + 429*4 + 411*1
-    p6 = int(round(p6 /16))
+    p3 = 408 * 1 + 372 * 4 + 480 * 6 + 444 * 4 + 447 * 1
+    p3 = int(round(p3 / 16))
 
-    p7 = 447*1 + 492*4 + 429*6 + 411*4 + 486*1
-    p7 = int(round(p7 /16))
+    p4 = 372 * 1 + 480 * 4 + 444 * 6 + 447 * 4 + 492 * 1
+    p4 = int(round(p4 / 16))
 
-    p8 = 492*1 + 429*4 + 411*6 + 486*4 + 486*1
-    p8 = int(round(p8 /16))
+    p5 = 480 * 1 + 444 * 4 + 447 * 6 + 492 * 4 + 429 * 1
+    p5 = int(round(p5 / 16))
 
-    p9 = 429*1 + 411*4 + 486*6 + 486*4 + 486*1
-    p9 = int(round(p9 /16))
-    
-    test = [ (0,p1), (1,p2), (2,p3), (3,p4), (4,p5), (5,p6), (6,p7), (7,p8), (8,p9)]
+    p6 = 444 * 1 + 447 * 4 + 492 * 6 + 429 * 4 + 411 * 1
+    p6 = int(round(p6 / 16))
+
+    p7 = 447 * 1 + 492 * 4 + 429 * 6 + 411 * 4 + 486 * 1
+    p7 = int(round(p7 / 16))
+
+    p8 = 492 * 1 + 429 * 4 + 411 * 6 + 486 * 4 + 486 * 1
+    p8 = int(round(p8 / 16))
+
+    p9 = 429 * 1 + 411 * 4 + 486 * 6 + 486 * 4 + 486 * 1
+    p9 = int(round(p9 / 16))
+
+    test = [(0, p1), (1, p2), (2, p3),
+            (3, p4), (4, p5), (5, p6),
+            (6, p7), (7, p8), (8, p9)]
     #print test
-    
+
     mav.addValue(0, 408)
     mav.addValue(1, 372)
     mav.addValue(2, 480)
@@ -176,7 +178,6 @@ if __name__ == '__main__':
     mav.addValue(7, 411)
     mav.addValue(8, 486)
 
-    
     mav.calcLast()
     #print mav.average
     if test == mav.average:
@@ -199,4 +200,3 @@ if __name__ == '__main__':
 #------------------------------------------------------------------------------
     time_end = time.time()
     print "end with no errors in: " + str(time_end - time_start)
-    
--- Analysis/plotGraph.py
+++ Analysis/plotGraph.py
@@ -3,44 +3,45 @@ Created on 19.02.2014
 
 @author: uwe
 '''
-import datetime
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
-import numpy as np
 import datetime as dt
 import time
 from MovingAverage import MovingAverage
 
+
 class ArgMismatch(Exception):
-     def __init__(self, value):
-         self.value = value
-     def __str__(self):
-         return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
-def plotGraph(list_of_list, diagramName = ''):
-    ''' [[xValues],[yValues]] 
+def plotGraph(list_of_list, diagramName=''):
+    ''' [[xValues],[yValues]]
         Thist Plotes all given values and a his filtert line.
     '''
-    if not len(list_of_list)%2 == 0:
+    if not len(list_of_list) % 2 == 0:
         raise ArgMismatch('Please provide [[xValues],[yValues], ...] as arguments')
-    
+
     time_start = time.time()
 #------------------------------------------------------------------------------
-    
-    oneHouer = 60*60
+
+    oneHouer = 60 * 60
     quater_day = oneHouer * 6
     half_a_day = oneHouer * 12
-    oneDay = oneHouer*24
-    oneWeek = oneDay*7
-    oneMonth = oneWeek*4
-    half_a_Year = oneMonth*6
-    one_Year = half_a_Year*2
+    oneDay = oneHouer * 24
+    oneWeek = oneDay * 7
+    oneMonth = oneWeek * 4
+    half_a_Year = oneMonth * 6
+    one_Year = half_a_Year * 2
     tmin = 0
 
-    now=time.mktime(time.localtime())
-    
-    for i in range(0,len(list_of_list),2):
+    now = time.mktime(time.localtime())
+
+    for i in range(0, len(list_of_list), 2):
         # ---- Calc filtered values ------
         list_length = len(list_of_list[i])
         if list_length > 21:
@@ -49,34 +50,34 @@ def plotGraph(list_of_list, diagramName = ''):
             mva = MovingAverage(list_length)
         for j in  range(0, len(list_of_list[i])):
             mva.addValue(list_of_list[i][j], list_of_list[i+1][j])
-        
+
         filterd_values = mva.getAverage()
         fvTimes = []
         fvValues = []
         for item in filterd_values:
             fvTimes.append(item[0])
             fvValues.append(item[1])
-        
+
         # ---- Append org values
         dates = [dt.datetime.fromtimestamp(ts) for ts in list_of_list[i]]
-        datenums=md.date2num(dates)
-        ax=plt.gca()
+        datenums = md.date2num(dates)
+        ax = plt.gca()
         xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
         ax.xaxis.set_major_formatter(xfmt)
-        plt.plot(datenums,list_of_list[i+1],lw=1, color='gray')
+        plt.plot(datenums, list_of_list[i + 1], lw=1, color='gray')
 
         # ---- Append filtered values ------
         dates = [dt.datetime.fromtimestamp(ts) for ts in fvTimes]
-        datenums=md.date2num(dates)
+        datenums = md.date2num(dates)
         tmin = min(datenums)
-        ax=plt.gca()
+        ax = plt.gca()
         xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
         ax.xaxis.set_major_formatter(xfmt)
-        plt.plot(datenums,fvValues,lw=2)
+        plt.plot(datenums, fvValues, lw=2)
 
     plt.subplots_adjust(bottom=0.2)
-    plt.xticks( rotation=90 )
-    ax.grid(True)  
+    plt.xticks(rotation=90)
+    ax.grid(True)
     plt.title(diagramName)
     #plt.text(tmin, -300, r'Read')
     #plt.text(tmin, +200, r'Write')
