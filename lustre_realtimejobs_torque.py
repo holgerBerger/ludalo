d715a214db508bfeffaf3155031532e804b7ed90
Holger Berger <holger.berger@googlemail.com>
2014-03-19 18:06:50 +0100

    parse logfiles as they are created, using inotify (needs patched version of python inotify interface)

---
+++ lustre_realtimejobs_torque.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import time
+import select
+import _inotify
+
+ROOTDIR="/var/spool/torque/server_priv/accounting"
+
+
+class Logfile:
+    def __init__(self, prefix, path, filename):
+        ''' path: directory where the file is
+            filename: name of file to read
+            prefix: filename has to start with this prefix
+        '''
+        self.path = path
+        self.prefix = prefix
+        self.filename = self.path+"/"+filename
+        self.f = open(self.filename,"r")
+        self.read_from_last_pos_to_end()
+
+    def read_from_last_pos_to_end(self):
+        '''read from file from current position to current end, build lists for inserts and updates
+        and do batch execution'''
+        jobstarts = {}
+        jobends = {}
+        b=self.f.read()
+        for l in b.split("\n"):
+            sp = l[:-1].split(";")
+            if len(sp)>1 and sp[1] in ["S","E"]:
+              datestr=sp[0]
+              if sp[1] == "S":
+                end=-1
+              else:
+                end=int(time.mktime(time.strptime(datestr,"%m/%d/%Y %H:%M:%S")))
+              jobid=sp[2]
+              fi = sp[3].split()
+              for i in fi:
+                if i.startswith("start"):
+                  start=i.split('=')[1]
+                if i.startswith("user"):
+                  owner=i.split('=')[1]
+                if i.startswith("exec_host"):
+                  l=[]
+                  for n in [x.split('/')[0] for x in  i.split('=')[1].split("+")]:
+                    if n not in l:
+                      l.append(n)
+                  hosts=",".join(l)
+                  if sp[1] == "S":
+                    jobstarts[jobid] = (jobid, start, end, owner, hosts, "") 
+                  else:
+                    jobends[jobid] = (jobid, start, end, owner, hosts, "")
+        inserts = []
+        updates = []
+        for i in jobstarts:
+            if i not in jobends:
+                inserts.append(jobstarts[i])
+            else:
+                inserts.append(jobends[i])
+        for i in jobends:
+            if i not in jobstarts:
+                updates.append(jobends[i])
+       
+        #FIXME do executemany here for insert and update
+        print inserts
+        print updates
+
+    def switch_file(self,filename):
+        # FIXME check for prefix + current date filename pattern, otherwise ignore it
+        self.read_from_last_pos_to_end()
+        self.f.close()
+        self.filename = self.path+"/"+filename
+        self.f = open(self.filename, "r")
+        #print "new file", self.filename
+
+    def action(self, e):
+        #print e
+        #print "changed"
+        if e["mask"] & _inotify.CREATE:
+            #print "create"
+            self.switch_file(e["name"])
+        if e["mask"] & _inotify.MODIFY:
+            #print "modify"
+            self.read_from_last_pos_to_end()
+        
+
+def mainloop():
+
+  fd = _inotify.create()
+  wddir = _inotify.add(fd, "watchdir", _inotify.CREATE | _inotify.MODIFY) 
+
+  todayfile = time.strftime("%Y%m%d")
+  lf = Logfile("","watchdir",todayfile)
+
+  while True:
+    # blocking wait
+    _inotify.read_event(fd, lf.action)
+    time.sleep(0.1)
+    
+
+if __name__ == "__main__":
+  mainloop() 
