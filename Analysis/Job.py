243300801cf1b489fbb0cf281b70aa16548c4471
Uwe Schilling <uweschillingbw@gmail.com>
2014-03-24 13:54:37 +0100

    implement some getter

--- Analysis/Job.py
+++ Analysis/Job.py
@@ -10,15 +10,46 @@ class Job(object):
     classdocs
     '''
 
-    def __init__(self, params):
+    def __init__(self, JobName, UserName):
         '''
         Constructor
         '''
-        self.Name = None
-        self.User = None
+        self.Name = JobName
+        self.User = UserName
         self.t_Start = None
         self.t_End = None
 
+        self.nidList = []
+
         self.WR_list = {}
         self.RD_list = {}
+        self.WQ_list = {}
         self.RQ_list = {}
+
+    def getReadList(self):
+        'returns read values'
+        returnList = []
+        for key in self.RD_list.keys():
+            returnList.append(self.RD_list.get(key))
+        return returnList
+
+    def getWriteList(self):
+        'returns read values'
+        returnList = []
+        for key in self.WR_list.keys():
+            returnList.append(self.WR_list.get(key))
+        return returnList
+
+    def getReadRequestList(self):
+        'returns read values'
+        returnList = []
+        for key in self.RQ_list.keys():
+            returnList.append(self.RQ_list.get(key))
+        return returnList
+
+    def getWriteRequestList(self):
+        'returns read values'
+        returnList = []
+        for key in self.WQ_list.keys():
+            returnList.append(self.WQ_list.get(key))
+        return returnList
