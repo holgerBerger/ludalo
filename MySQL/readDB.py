db895f0a8a6992a0023f02e794206898be04b871
Uwe Schilling <uweschillingbw@gmail.com>
2014-04-22 09:13:37 +0200

    apend io polt to filesystem

--- MySQL/readDB.py
+++ MySQL/readDB.py
@@ -512,14 +512,15 @@ class readDB(object):
         list_of_list = []
         list_of_list.append(values_np[:, 0])  # time
         list_of_list.append(values_np[:, 1] / (60 * 1000000))  # wb
-        #list_of_list.append(values_np[:, 0])  # time
-        #list_of_list.append(values_np[:, 2])  # wio
         list_of_list.append(values_np[:, 0])  # time
         list_of_list.append(0 - (values_np[:, 3] / (60 * 1000000)))  # rb
-        #list_of_list.append(values_np[:, 0])  # time
-        #list_of_list.append(values_np[:, 4])  # rio
-
         plotGraph(list_of_list, fs, 21)
+
+        list_of_list.append(values_np[:, 0])  # time
+        list_of_list.append(values_np[:, 4])  # rio
+        list_of_list.append(values_np[:, 0])  # time
+        list_of_list.append(values_np[:, 2])  # wio
+        plotGraph(list_of_list, fs + 'io', 21)
         print 'done'
         exit()
 #------------------------------------------------------------------------------
