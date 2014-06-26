e7782b1e35e13d7debadf27434142a167f4f574c
Uwe Schilling <uweschillingbw@gmail.com>
2014-06-26 16:33:15 +0200

    fix for
    Traceback (most recent call last):
      File "readDB.py", line 664, in <module>
        db.print_all_jobs()
      File "readDB.py", line 624, in print_all_jobs
        self.print_job(job[1])
      File "readDB.py", line 501, in print_job
        if values_np:
    ValueError: The truth value of an array with more than one element is
    ambiguous. Use a.any() or a.all()

--- MySQL/readDB.py
+++ MySQL/readDB.py
@@ -498,7 +498,7 @@ class readDB(object):
 
         values_np = self.query_to_npArray(query, option)
 
-        if values_np:
+        if values_np.any():
             query = ''' select
                             c_timestamp
                         from
