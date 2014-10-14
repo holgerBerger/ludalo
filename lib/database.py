c650146c931d610bbf4241e6f2372441e65ef6c2
Uwe Schilling <uweschillingbw@gmail.com>
2014-10-14 09:08:03 +0200

    bugfix

--- lib/database.py
+++ lib/database.py
@@ -132,7 +132,8 @@ class DatabaseInserter(multiprocessing.Process):
         # build hostmap
         self.nidMap = self.readhostfile()
 
-        while not self.exit.is_set() and self.comQueue.empty():
+        # self.exit.is_set() fals until exit
+        while not (self.exit.is_set() and self.comQueue.empty()):
             while self.comQueue.empty():
                 time.sleep(0.1)
 
